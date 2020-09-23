a1978afb78e305ef461293b0a4bee37edbcfa073
bitlucky <lokeshtiwari877@gmail.com>
2020-09-23 10:51:31 +0530

    Bit Manipulation

---
+++ BitMagic/1.evenodd.cpp
@@ -0,0 +1,11 @@
+// The idea is to check whether the last bit of the number is set or not.
+//  If last bit is set then the number is odd, otherwise even.
+
+// As we know bitwise AND Operation of the Number by 1 will be 1, 
+// If it is odd because the last bit will be already set. Otherwise it will give 0 as output.
+
+bool isEven(int n)  
+{  
+    // n&1 is 1, then odd, else even  
+    return (!(n & 1));  
+}  
